3e794962a37300fea66edea70b1e3a7f1a98005f
Vincenzo Junior Striano <vincenzojunior@Mac.station>
2026-04-01 16:19:18 +0200

    minor changes in main

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from CleanerScraper import CleanerScraper
 
-
 def main():
     print("Hello from financial-advisor!")
 
     scraper = CleanerScraper("https://www.italiapersonalfinance.it")
     scraper.create_sitemap()
     results = scraper.scrape()
-
-    print()
-    print(results[0])
-    print()
-    print(results[0]["url"])
-    print()
-    print(results[0]["content"])
-
 
 if __name__ == "__main__":
     main()
