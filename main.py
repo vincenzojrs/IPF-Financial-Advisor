cbc2224af29e9ba5decd87fbc496417255cea7ef
vincenzojrs <vjrstriano@gmail.com>
2026-04-14 20:21:17 +0200

    implemented a CleanerScraper class, containing sitemap creation using tavilymap, html content extraction and cleaning using beautiful soup, error handling, pep-8 docstring

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from CleanerScraper import CleanerScraper
+
+
 def main():
     print("Hello from financial-advisor!")
+
+    scraper = CleanerScraper("https://www.italiapersonalfinance.it")
+    scraper.create_sitemap()
+    results = scraper.scrape()
+
+    print()
+    print(results[0])
+    print()
+    print(results[0]["url"])
+    print()
+    print(results[0]["content"])
 
 
 if __name__ == "__main__":
